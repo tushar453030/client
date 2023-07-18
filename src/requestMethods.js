79542049c31f84b7444e77da11f0dcbf733c78bc
tushar453030 <93340408+tushar453030@users.noreply.github.com>
2023-07-18 13:20:43 +0530

    ch

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "https://server-vercel-production.up.railway.app/api/";
-const TOKEN =JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken || "";
 
-// const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-// const currentUser = user && JSON.parse(user).currentUser;
-
+const userStorageData = JSON.parse(localStorage.getItem("persist:root"));
+const TOKEN =
+  userStorageData && userStorageData.user && userStorageData.user.currentUser
+    ? userStorageData.user.currentUser.accessToken
+    : "";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -13,5 +14,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
