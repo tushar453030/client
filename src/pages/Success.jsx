4910ea5a3e190078d5541529a52e96948e75377f
Tushar Sharma <tushar4530@gmail.com>
2024-05-15 00:41:59 +0530

    success page strip[e

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Success = () => {
-  return (
-    <div>Success</div>
-  )
+  const { data } = useLocation()
+  console.log(data)
+  return <div>Success</div>
 }
 
-export default Success;
+export default Success
