c8b150f83fec838ba2c2171cc8423f92362606b6
Tushar Sharma <tushar4530@gmail.com>
2024-05-15 00:12:17 +0530

    fixes ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,27 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import {Search, ShoppingCartOutlined} from "@material-ui/icons";
-import { Badge } from '@material-ui/core';
-import {mobile} from "../responsive";
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from "react-router-dom";
-import { logout } from '../redux/apiCalls';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+import { mobile } from '../responsive'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logout } from '../redux/apiCalls'
 
 const Container = styled.div`
-    height: 60px;
-    ${mobile({height: "50px"})}
+  height: 60px;
+  ${mobile({ height: '50px' })}
 `
 
 const Wrapper = styled.div`
-    padding:10px 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    ${mobile({padding: "10px 0px"})}
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ${mobile({ padding: '10px 0px' })}
 `
 
-const Left =styled.div`
+const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
@@ -31,7 +31,7 @@ const Language = styled.span`
   font-size: 14px;
   color: black;
   cursor: pointer;
-  ${mobile({display: "none"})}
+  ${mobile({ display: 'none' })}
 `
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -39,87 +39,83 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
-`;
+`
 const Input = styled.input`
   border: none;
-  ${mobile({ width: "50px" })}
-`;
+  ${mobile({ width: '50px' })}
+`
 
-const Center= styled.div`
+const Center = styled.div`
   flex: 1;
   text-align: center;
-`;
+`
 
 const Logo = styled.h1`
   font-weight: bold;
-  ${mobile({ fontSize: "24px"})}
-`;
-const Right= styled.div`
+  ${mobile({ fontSize: '24px' })}
+`
+const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  ${mobile({ flex: 2,justifyContent: "center"})}
-`;
+  ${mobile({ flex: 2, justifyContent: 'center' })}
+`
 
 const MenuItem = styled.div`
   font-display: 14px;
   color: black;
   cursor: pointer;
   margin-left: 25px;
-  ${mobile({fontSize: "12px", marginLeft: "10px"})}
+  ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `
 const Navbar = () => {
-  const quantity = useSelector(state=>state.cart.quantity);
-  const currentUser = useSelector((state) => state.user.currentUser);
-  const navigate = useNavigate(); 
-  const dispatch= useDispatch();
-  const { isFetching, error } = useSelector((state) => state.user);
+  const quantity = useSelector((state) => state.cart.quantity)
+  const currentUser = useSelector((state) => state.user.currentUser)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { isFetching, error } = useSelector((state) => state.user)
 
   const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
-  };
-  
-  
-
+    e.preventDefault()
+    dispatch(logout())
+  }
 
   return (
     <Container>
       <Wrapper>
         <Left>
-        <Link to="/" style={{ textDecoration: 'none' }}><Language>Shope2nd</Language></Link>
-          <SearchContainer>
-            <Input placeholder='search'/>
-            <Search style={{color:"gray", fontSize:16}}/>
-          </SearchContainer>
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <Language>Shope2nd</Language>
+          </Link>
         </Left>
 
-
-        <Link to="/" style={{ textDecoration: 'none' }}><Center><Logo>Shop2nd</Logo></Center></Link>
-
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <Center>
+            <Logo>Shop2nd</Logo>
+          </Center>
+        </Link>
 
         <Right>
-        {currentUser ? (
+          {currentUser ? (
             <MenuItem onClick={handleLogout}>LOGOUT</MenuItem>
           ) : (
             <>
-              <Link to="/register" style={{ textDecoration: 'none' }}>
+              <Link to='/register' style={{ textDecoration: 'none' }}>
                 <MenuItem>REGISTER</MenuItem>
               </Link>
-              <Link to="/login" style={{ textDecoration: 'none' }}>
+              <Link to='/login' style={{ textDecoration: 'none' }}>
                 <MenuItem>SIGN IN</MenuItem>
               </Link>
             </>
           )}
 
-
-          <Link to="/cart" >
-          <MenuItem>
-            <Badge badgeContent={quantity} color="primary">
-              <ShoppingCartOutlined/>
-            </Badge>
-          </MenuItem>
+          <Link to='/cart'>
+            <MenuItem>
+              <Badge badgeContent={quantity} color='primary'>
+                <ShoppingCartOutlined />
+              </Badge>
+            </MenuItem>
           </Link>
         </Right>
       </Wrapper>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import { mobile } from "../responsive";
-import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
-import { login } from "../redux/apiCalls";
-import { Link } from "react-router-dom";
+import styled from 'styled-components'
+import { mobile } from '../responsive'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+import { login } from '../redux/apiCalls'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
   width: 100vw;
@@ -12,42 +12,42 @@ const Container = styled.div`
       rgba(255, 255, 255, 0.5),
       rgba(255, 255, 255, 0.5)
     ),
-    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+    url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
       center;
   background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: white;
-  ${mobile({ width: "75%" })}
-`;
+  ${mobile({ width: '75%' })}
+`
 
 const Title = styled.h1`
   font-size: 24px;
   font-weight: 300;
-`;
+`
 
 const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
-`;
+`
 
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
   margin: 20px 10px 0px 0px;
   padding: 10px;
-`;
+`
 
 const Agreement = styled.span`
   font-size: 12px;
   margin: 20px 0px;
-`;
+`
 
 const Button = styled.button`
   width: 40%;
@@ -60,49 +60,51 @@ const Button = styled.button`
     color: green;
     cursor: not-allowed;
   }
-`;
+`
 
 const Error = styled.span`
   color: red;
-`;
-
+`
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const { isFetching, error } = useSelector((state) => state.user);
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const { isFetching, error } = useSelector((state) => state.user)
 
   const handleClick = (e) => {
-    e.preventDefault();
-    login(dispatch, { username, password });
-  };
+    e.preventDefault()
+    login(dispatch, { username, password })
+  }
   return (
     <Container>
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          
-          
-          
-          <Input placeholder="username"
-            onChange={(e) => setUsername(e.target.value)} />
-          <Input placeholder="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}/>
-          
+          <Input
+            placeholder='username'
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder='password'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button onClick={handleClick} disabled={isFetching}>Login</Button>
+          <Button onClick={handleClick} disabled={isFetching}>
+            Login
+          </Button>
           {error && <Error>Something went wrong...</Error>}
           <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
